14a8b89facce4326b8dd2201af05eb284b9e17ab
Pedro Yanes Garrido <pedro.yanes@edu.uah.es>
2023-01-06 00:25:22 +0100

    Challenge #22 added!!

---
+++ Solutions/Challenge #22/challenge-22.js
@@ -0,0 +1,18 @@
+function checkStepNumbers(systemNames, stepNumbers) {
+    const uniq = [...new Set(systemNames)]
+    let numeros = []
+    for (let i = 0; i < uniq.length; i++) {
+      for (let j = 0; j < systemNames.length; j++) {
+        if (systemNames[j] === uniq[i])
+          numeros.push(stepNumbers[j])
+      }
+      for (let k = 0; k < numeros.length - 1; k++) {
+        if (numeros[i] >= numeros[i + 1]) return false
+      }
+      numeros = []
+    }
+    return true      
+}
+
+//NOTE: It works but I don't understand why here ""if (numeros[i] >= numeros[i + 1]) return false""
+//it works with "">="", I think it should only work with "">"" but it doesn't...  SMH :(
